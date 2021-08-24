4b48ad4cba49849d8efbd4a3323a4d09edf88aac
Bryan <brymso@gmail.com>
2021-08-24 01:05:17 -0500

    composer config

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc761297626c08df39210712a5167e608
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc761297626c08df39210712a5167e608', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc761297626c08df39210712a5167e608', 'loadClassLoader'));
